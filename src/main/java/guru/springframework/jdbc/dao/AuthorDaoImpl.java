04a40c4ff91a54d93b05eea3b516f916790103ab
Nicu Taban <nictbndeveloper@gmail.com>
2022-04-18 22:54:16 +0300

    Prepared statements example

--- src/main/java/guru/springframework/jdbc/dao/AuthorDaoImpl.java
+++ src/main/java/guru/springframework/jdbc/dao/AuthorDaoImpl.java
@@ -4,10 +4,7 @@ import guru.springframework.jdbc.domain.Author;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 @Component
 public class AuthorDaoImpl implements AuthorDao {
@@ -21,13 +18,14 @@ public class AuthorDaoImpl implements AuthorDao {
     @Override
     public Author getById(Long id) {
         Connection connection = null;
-        Statement statement = null;
+        PreparedStatement preparedStatement = null;
         ResultSet resultSet = null;
 
         try {
             connection = source.getConnection();
-            statement = connection.createStatement();
-            resultSet = statement.executeQuery("Select * From author where id = " + id);
+            preparedStatement = connection.prepareStatement("Select * From author where id = ?");
+            preparedStatement.setLong(1, id);
+            resultSet = preparedStatement.executeQuery();
 
             if (resultSet.next()) {
                 Author author = new Author();
@@ -43,8 +41,8 @@ public class AuthorDaoImpl implements AuthorDao {
                 if (resultSet != null) {
                     resultSet.close();
                 }
-                if (statement != null) {
-                    statement.close();
+                if (preparedStatement != null) {
+                    preparedStatement.close();
                 }
                 if (connection != null) {
                     connection.close();
